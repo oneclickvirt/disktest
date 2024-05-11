72b4d28850f18df6821048c9222d69756beb020d
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-05-11 03:52:24 +0000

    update

--- disktest/disktest.go
+++ disktest/disktest.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"github.com/shirou/gopsutil/disk"
@@ -127,36 +128,84 @@ func DDTest(language string, enableMultiCheck bool) string {
 		}
 	}
 	if enableMultiCheck {
+		if language == "zh" {
+			result += "测试盘        块大小             直接写入                           直接读取\n"
+		} else {
+			result += "Test Disk    Block Size         Direct Write                      Direct Read\n"
+		}
 		for index, path := range mountPoints {
+			// 写入测试
 			// dd if=/dev/zero of=/tmp/100MB.test bs=4k count=25600 oflag=direct
-			cmd := exec.Command("dd", "if=/dev/zero", "of="+path+"/100MB.test", "bs=4k", "count=25600", "oflag=direct")
-			stderr, err := cmd.StderrPipe()
+			cmd1 := exec.Command("dd", "if=/dev/zero", "of="+path+"/100MB.test", "bs=4k", "count=25600", "oflag=direct")
+			stderr1, err := cmd1.StderrPipe()
 			if err == nil {
-				if language == "zh" {
-					result += "测试盘                  直接写入                 直接读取\n"
-				} else {
-					result += "Test Disk               Direct Read             Direct Read\n"
-				}
-				if err := cmd.Start(); err == nil {
-					outputBytes, err := io.ReadAll(stderr)
+				result += fmt.Sprintf("%-10s", strings.TrimSpace(devices[index])) + "    " + fmt.Sprintf("%-15s", "100MB-4K Block") + "    "
+				if err := cmd1.Start(); err == nil {
+					outputBytes, err := io.ReadAll(stderr1)
 					if err == nil {
 						tempText := string(outputBytes)
 						tp1 := strings.Split(tempText, "\n")
+						var records, usageTime float64
+						records, _ = strconv.ParseFloat(strings.Split(strings.TrimSpace(tp1[0]), "+")[0], 64)
 						for _, t := range tp1 {
 							if strings.Contains(t, "bytes") {
 								// t 为 104857600 bytes (105 MB, 100 MiB) copied, 4.67162 s, 22.4 MB/s
 								tp2 := strings.Split(t, ",")
 								if len(tp2) == 4 {
-									blockSize := strings.Split(strings.TrimSpace(tp2[0]), " ")[0]
-									usageTime := strings.Split(strings.TrimSpace(tp2[2]), " ")[0]
+									usageTime, _ = strconv.ParseFloat(strings.Split(strings.TrimSpace(tp2[2]), " ")[0], 64)
 									ioSpeed := strings.Split(strings.TrimSpace(tp2[3]), " ")[0]
-									fmt.Println(devices[index], blockSize, usageTime, ioSpeed)
+									iops := records / usageTime
+									var iopsText string
+									if iops >= 1000 {
+										iopsText = strconv.FormatFloat(iops/1000, 'f', 2, 64) + "K IOPS, " + strconv.FormatFloat(usageTime, 'f', 2, 64) + "s"
+									} else {
+										iopsText = strconv.FormatFloat(iops, 'f', 2, 64) + " IOPS, " + strconv.FormatFloat(usageTime, 'f', 2, 64) + "s"
+									}
+									result += fmt.Sprintf("%-30s", strings.TrimSpace(ioSpeed)+" MB/s("+iopsText+")") + "    "
 								}
 							}
 						}
-						// IOPS （每秒输入/输出操作数），通过写入的块数除以写入所用的时间来计算
-						// 测试盘        测试操作		         写速度					             读速度
-						// /dev/sdb1    100MB-4K Block		37.6 MB/s (9173 IOPS, 2.79s)		51.3 MB/s (12519 IOPS, 2.04s)
+					}
+				}
+			}
+			// 读取测试
+			// dd if=/tmp/100MB.test of=/dev/null bs=4k count=25600 oflag=direct
+			nullPath := "/dev/null"
+			_, err = os.Stat(nullPath)
+			var cmd2 *exec.Cmd
+			if os.IsNotExist(err) {
+				cmd2 = exec.Command("dd", "if="+path+"/100MB.test", "of="+path+"/100MB_read.test", "bs=4k", "count=25600", "oflag=direct")
+			} else {
+				cmd2 = exec.Command("dd", "if="+path+"/100MB.test", "of=/dev/null", "bs=4k", "count=25600", "oflag=direct")
+			}
+			stderr2, err := cmd2.StderrPipe()
+			if err == nil {
+				if err := cmd2.Start(); err == nil {
+					outputBytes, err := io.ReadAll(stderr2)
+					if err == nil {
+						tempText := string(outputBytes)
+						fmt.Println("1", tempText)
+						tp1 := strings.Split(tempText, "\n")
+						var records, usageTime float64
+						records, _ = strconv.ParseFloat(strings.Split(strings.TrimSpace(tp1[0]), "+")[0], 64)
+						for _, t := range tp1 {
+							if strings.Contains(t, "bytes") {
+								// t 为 104857600 bytes (105 MB, 100 MiB) copied, 4.67162 s, 22.4 MB/s
+								tp2 := strings.Split(t, ",")
+								if len(tp2) == 4 {
+									usageTime, _ = strconv.ParseFloat(strings.Split(strings.TrimSpace(tp2[2]), " ")[0], 64)
+									ioSpeed := strings.Split(strings.TrimSpace(tp2[3]), " ")[0]
+									iops := records / usageTime
+									var iopsText string
+									if iops >= 1000 {
+										iopsText = strconv.FormatFloat(iops/1000, 'f', 2, 64) + "K IOPS, " + strconv.FormatFloat(usageTime, 'f', 2, 64) + "s"
+									} else {
+										iopsText = strconv.FormatFloat(iops, 'f', 2, 64) + " IOPS, " + strconv.FormatFloat(usageTime, 'f', 2, 64) + "s"
+									}
+									result += fmt.Sprintf("%-30s", strings.TrimSpace(ioSpeed)+" MB/s("+iopsText+")") + "    "
+								}
+							}
+						}
 					}
 				}
 			}
@@ -179,6 +228,7 @@ func DDTest(language string, enableMultiCheck bool) string {
 	//25600+0 records out
 	//104857600 bytes (105 MB, 100 MiB) copied, 15.034 s, 7.0 MB/s
 	// dd if=/dev/zero of=/root/1GB.test bs=1M count=1000 oflag=direct
+
 	//1000+0 records in
 	//1000+0 records out
 	//1048576000 bytes (1.0 GB, 1000 MiB) copied, 2.7358 s, 383 MB/s
