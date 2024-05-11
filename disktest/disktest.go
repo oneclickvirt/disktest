b8da93b6b9ba518859fae078bd00a06d6cdec3af
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-05-11 15:42:24 +0000

    update

--- disktest/disktest.go
+++ disktest/disktest.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"runtime"
 	"strings"
 
 	"github.com/shirou/gopsutil/disk"
@@ -73,6 +74,7 @@ func DDTest(language string, enableMultiCheck bool) string {
 					}
 				}
 			}
+			os.Remove(path + "/100MB.test")
 			// 读取测试
 			// dd if=/tmp/100MB.test of=/dev/null bs=4k count=25600 oflag=direct
 			cmd2 := exec.Command("dd", "if="+path+"/100MB.test", "of=/dev/null", "bs=4k", "count=25600", "oflag=direct")
@@ -101,6 +103,8 @@ func DDTest(language string, enableMultiCheck bool) string {
 					}
 				}
 			}
+			os.Remove(path + "/100MB.test")
+			os.Remove(path + "/100MB_read.test")
 			result += "\n"
 		}
 	} else {
@@ -175,3 +179,55 @@ func DDTest(language string, enableMultiCheck bool) string {
 //1000+0 records in
 //1000+0 records out
 //1048576000 bytes (1.0 GB, 1000 MiB) copied, 2.7358 s, 383 MB/s
+
+// FioTest 通过fio测试硬盘
+func FioTest(language string, enableMultiCheck bool) string {
+	var (
+		result, fioSize string
+		devices         []string
+		mountPoints     []string
+	)
+	parts, err := disk.Partitions(false)
+	if err == nil {
+		for _, f := range parts {
+			if !strings.Contains(f.Device, "vda") && !strings.Contains(f.Device, "snap") && !strings.Contains(f.Device, "loop") {
+				if isWritableMountpoint(f.Mountpoint) {
+					devices = append(devices, f.Device)
+					mountPoints = append(mountPoints, f.Mountpoint)
+				}
+			}
+		}
+	}
+	if language == "en" {
+		result += "Test Path    Block Size         4k (IOPS)                        64k(IOPS)\n"
+	} else {
+		result += "测试路径      块大小             4k (IOPS)                        64k(IOPS)\n"
+	}
+	// 生成测试文件
+	if runtime.GOARCH == "amd64" || runtime.GOARCH == "x86" || runtime.GOARCH == "x86_64" {
+		fioSize = "2G"
+	} else {
+		fioSize = "512MB"
+	}
+	if enableMultiCheck {
+		for index, path := range mountPoints {
+			// 生成对应文件
+			// fio --name=setup --ioengine=libaio --rw=read --bs=64k --iodepth=64 --numjobs=2 --size=512MB --runtime=1 --gtod_reduce=1 --filename="/tmp/test.fio" --direct=1 --minimal
+			cmd1 := exec.Command("fio", "--name=setup", "--ioengine=libaio", "--rw=read", "--bs=64k", "--iodepth=64", "--numjobs=2", "--size="+fioSize, "--runtime=1", "--gtod_reduce=1",
+				"--filename=\""+path+"/test.fio\"", "--direct=1", "--minimal")
+			defer os.Remove(path + "/test.fio")
+			stderr1, err := cmd1.StderrPipe()
+			if err == nil {
+				// result += fmt.Sprintf("%-10s", strings.TrimSpace(devices[index])) + "    " + fmt.Sprintf("%-15s", "100MB-4K Block") + "    "
+				// if err := cmd1.Start(); err == nil {
+				// 	outputBytes, err := io.ReadAll(stderr1)
+				// 	if err == nil {
+				// 		tempText := string(outputBytes)
+				// 		result += parseResultDD(tempText)
+				// 	}
+				// }
+			}
+		}
+	}
+	return result
+}
